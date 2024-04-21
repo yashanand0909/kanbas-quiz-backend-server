f3d7fb2afb8707f9ca3d24e8a25f670ade0cf4f8
Yash Anand <yashanand.0909@gmail.com>
2024-04-21 17:06:19 -0400

    deploment changes

--- App.js
+++ App.js
@@ -11,29 +11,26 @@ import CourseRoutes from "./Kanbas/courses/routes.js";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
 mongoose.connect(CONNECTION_STRING);
 const app = express();
-app.use(express.json());
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:3000'
+    origin: process.env.FRONTEND_URL
     }
 ));
-// app.use(express.json());
-const sessionOptions = {
-    secret: "MySecretKey",
-    resave: false,
-    saveUninitialized: false,
-  };  
-  if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-      sameSite: "none",
-      secure: true,
-      domain: process.env.HTTP_SERVER_DOMAIN,
-    };
-  }
+app.use(express.json());
+
   app.use(
-    session(sessionOptions)
-  );  
+      session({
+        secret: "keyboard cat",
+        resave: false,
+        saveUninitialized: false,
+        proxy: true,
+        cookie: {
+          sameSite: "none",
+          secure: true,
+          domain: "kanbas-server-app-project.onrender.com",
+        },
+      })
+    ); 
 
 ModuleRoutes(app);
 CourseRoutes(app);
